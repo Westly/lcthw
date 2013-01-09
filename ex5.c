c4ae5676fb7cd18af0d7008bdec2fa0b85753ac7
random <atreyuroc@gmail.com>
2013-01-08 23:55:37 -0800

    Adding exercise 5

---
+++ ex5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* This is a comment. */
+
+// Why have int names argc, and a character names argv? Whats with the square brackets?
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+
+	// This is also a comment
+	printf("You are %d miles away.\n", distance);
+
+	return 0;
+}
